5bd2e7fcbbeac70628412f0f7c935b8901d24e4c
Sergey Stasishin <s.stasishin@gmail.com>
2012-11-22 20:05:43 +0400

    "@Order from" form

--- src/com/taxisoft/taxiorder/OrderFromActivity.java
+++ src/com/taxisoft/taxiorder/OrderFromActivity.java
@@ -3,13 +3,24 @@ package com.taxisoft.taxiorder;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class OrderFromActivity extends Activity {
 
+	private static final String[] COUNTRIES = new String[] {
+        "Belgium", "France", "Italy", "Germany", "Spain", "Finland"
+    };
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_order_from);
+		
+	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+	    AutoCompleteTextView textView = (AutoCompleteTextView)findViewById(R.id.edtStreetFrom);
+	    textView.setAdapter(adapter);
+	    textView.setThreshold(1);
 	}
 
 	@Override
